fc046ce64fad364cdd44229edd6cd68b7e86fdc0
jonki <andrzej.fricze@gmail.com>
2021-02-10 23:06:51 +0100

    Distance to create velocity

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ struct Ball {
 }
 
 struct MainState {
-    move_val: f32,
+    horizontal_move: f32,
+    vertical_move: f32,
     ball: Ball,
     paddle_left: Paddle,
     paddle_right: Paddle,
@@ -27,7 +28,8 @@ impl MainState {
             paddle_left: Paddle { x: 0.0, y: 0.0 },
             paddle_right: Paddle { x: x_right, y: 0.0 },
             ball: Ball { x: 0.0, y: 300.0 },
-            move_val: 3.0,
+            horizontal_move: 3.0,
+            vertical_move: 0.0,
         };
         Ok(s)
     }
@@ -45,13 +47,16 @@ impl event::EventHandler for MainState {
         let right_paddle_collision =
             self.ball.y <= self.paddle_right.y + 40.0 && self.ball.y >= self.paddle_right.y - 10.0;
 
+        let right_paddle_distance = self.ball.y - self.paddle_right.y - 18.0;
+        let left_paddle_distance = self.ball.y - self.paddle_left.y;
+
         if right_wall_collision && right_paddle_collision {
-            self.move_val = -3.0;
+            self.horizontal_move = -3.0;
         } else if left_wall_collision && left_paddle_collision {
-            self.move_val = 3.0;
+            self.horizontal_move = 3.0;
         }
 
-        self.ball.x = self.ball.x % 800.0 + self.move_val;
+        self.ball.x = self.ball.x % 800.0 + self.horizontal_move;
 
         Ok(())
     }
@@ -64,7 +69,7 @@ impl event::EventHandler for MainState {
             graphics::DrawMode::fill(),
             Vec2::new(0.0, 0.0),
             10.0,
-            2.0,
+            1.0,
             graphics::WHITE,
         )?;
 
